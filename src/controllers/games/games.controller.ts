4acd93eac8d9033e2af66c9718381ad0755989a5
William Coulter <4williamcoulter4@gmail.com>
2022-03-08 22:23:34 +1100

    WIP create game

--- src/controllers/games/games.controller.ts
+++ src/controllers/games/games.controller.ts
@@ -1,11 +1,35 @@
-import { Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
+import { PlayersService } from "../players/players.service";
+import { GameCreateDto } from "./interfaces/game-create.interface";
 
 @Controller("games")
 export class GamesController {
-  constructor() {}
+  constructor(private readonly playerService: PlayersService) {}
 
   @Post("create")
-  create(): string {
+  async create(@Body() dto: GameCreateDto): Promise<string> {
+    const { player1, player1Score, player2, player2Score } = dto;
+
+    if (!(await this.playerService.isPlayer(player1))) {
+      throw new HttpException(
+        `Player ${player1} is not a registered player`,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    if (!(await this.playerService.isPlayer(player2))) {
+      throw new HttpException(
+        `Player ${player2} is not a registered player`,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
     return "This endpoint creates a played game";
   }
 }
---
+++ src/controllers/games/interfaces/game-create.interface.ts
@@ -0,0 +1,6 @@
+export interface GameCreateDto {
+  player1: string;
+  player2: string;
+  player1Score: number;
+  player2Score: number;
+}
