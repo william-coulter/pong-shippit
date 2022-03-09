b02afadffc78c870e5eadbf0636cf1c97db5f1c2
William Coulter <4williamcoulter4@gmail.com>
2022-03-09 21:31:22 +1100

    I think this schema makes the most sense

--- prisma/migrations/20220309103021_init/migration.sql
+++ prisma/migrations/20220309103021_init/migration.sql
@@ -11,30 +11,16 @@ CREATE TABLE "players" (
 CREATE TABLE "games" (
     "id" SERIAL NOT NULL,
     "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "winnerName" TEXT NOT NULL,
+    "loserName" TEXT NOT NULL,
     "winningScore" INTEGER NOT NULL,
     "losingScore" INTEGER NOT NULL,
-    "winnerName" TEXT NOT NULL,
 
     CONSTRAINT "games_pkey" PRIMARY KEY ("id")
 );
-
--- CreateTable
-CREATE TABLE "_GameToPlayer" (
-    "A" INTEGER NOT NULL,
-    "B" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "_GameToPlayer_AB_unique" ON "_GameToPlayer"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_GameToPlayer_B_index" ON "_GameToPlayer"("B");
 
 -- AddForeignKey
 ALTER TABLE "games" ADD CONSTRAINT "games_winnerName_fkey" FOREIGN KEY ("winnerName") REFERENCES "players"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_GameToPlayer" ADD FOREIGN KEY ("A") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_GameToPlayer" ADD FOREIGN KEY ("B") REFERENCES "players"("name") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "games" ADD CONSTRAINT "games_loserName_fkey" FOREIGN KEY ("loserName") REFERENCES "players"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
